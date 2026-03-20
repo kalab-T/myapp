835d3d1b768f6d4888941b4d0fabc282361f5277
Kalab-T <ktesfaneh2@gmail.com>
2026-03-20 12:59:49 -0700

    Use explicit telegram API route for deployment debugging

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' }
+      ]
+    }
+  },
   runtimeConfig: {
     telegramBotToken: '',
     telegramChatId: ''
